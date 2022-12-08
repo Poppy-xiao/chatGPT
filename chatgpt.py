a76ae15925d081991fc82f77280f66bbe845f173
Poppy <xiaoyufeng639@gmail.com>
2022-12-08 12:49:54 +0900

    update session to keep one conversation

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,12 @@
 from hoshino import Service, priv 
 import asyncio
 from revChatGPT.revChatGPT import Chatbot
+import time
 
 config = {
         "Authorization": "<Your Bearer Token Here>", # This is optional
         "session_token": "<Your Token Here>"
-}
+        }
 
 user_session = dict()
 chatbot = Chatbot(config)
@@ -22,13 +23,27 @@ sv = Service(
     help_=sv_help  # 帮助说明
 ) 
 
-def get_chat_response(prompt): 
+
+def get_chat_response(session_id, prompt):
+    if session_id in user_session:
+        # 如果在三分钟内再次发起对话则使用相同的会话ID
+        if time.time() < user_session[session_id]['timestamp'] + 60 * 3:
+            chatbot.conversation_id = user_session[session_id]['conversation_id']
+            chatbot.parent_id = user_session[session_id]['parent_id']
+        else:
+            chatbot.reset_chat()
+    else:
+        chatbot.reset_chat() 
     try:
-        chatbot.reset_chat()
-        resp = chatbot.get_chat_response(prompt)
+        resp = chatbot.get_chat_response(prompt, output="text") 
+        user_cache = dict()
+        user_cache['timestamp'] = time.time()
+        user_cache['conversation_id'] = resp['conversation_id']
+        user_cache['parent_id'] = resp['parent_id']
+        user_session[session_id] = user_cache
+
         return resp['message']
     except Exception as e:
-        print(e)
         return f"发生错误: {str(e)}"
  
 
@@ -38,6 +53,10 @@ async def chatGPT_method(bot, ev):
     gid = ev.group_id
     name = ev.sender['nickname'] 
     msg = str(ev.message.extract_plain_text()).strip()
-    resp = await asyncio.get_event_loop().run_in_executor(None, get_chat_response, msg)
+    resp = await asyncio.get_event_loop().run_in_executor(None, get_chat_response, uid, msg)
     await bot.send(ev, resp, at_sender = True)
- 
+
+ # 定时刷新seesion_token
+@sv.scheduled_job("interval", minutes=10)
+async def refresh_session(): 
+    chatbot.refresh_session()
